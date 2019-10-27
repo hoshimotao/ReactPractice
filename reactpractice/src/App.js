06887e96938d1f783de4f03bebc28b59c96bae84
Hoshimotao <jthomas302@gmail.com>
2019-10-27 19:37:34 -0400

    organize program

---
+++ reactpractice/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <ul>
+        <li> First list item </li>
+        <li> Second list item </li>
+        <li> Third list item </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
