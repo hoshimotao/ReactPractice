02e343a112f7775144d2fc9d81dc0729ae24ae79
Hoshimotao <jthomas302@gmail.com>
2019-10-29 23:56:30 -0400

    add function to checkboxes

--- reactpractice/src/components/Checkboxes.js
+++ reactpractice/src/components/Checkboxes.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
 
-function handleChange({target}){
-  if (target.checked){
-     target.parentNode = "Complete"
-     
-  }
-}
+
 
 
 export default class Checkboxes extends Component {
-
-
   render() {
+
+
+function onChange(){
+    var isChecked = document.getElementById("firstCheck").innerText
+
+    console.log(isChecked)
+}
+
+  
 
     return (
       <div>
 
         <h3> To Do List </h3>
 <ul>
+      
+       <li className="checkboxComplete"><input className="checkedBox" type="checkbox" onChange={this.onChange} /><label id="firstCheck"> Apply to Job 1 </label></li> 
+       <li className="done"><input className="checkedBox" type="checkbox" /> Done </li>
 
-       <li className="checkboxComplete"><input className="checkedBox" type="checkbox" default= "unchecked" onClick={this.onChange}/> Apply to Job 1 </li>
-        <li className="checkboxComplete"><input className="checkedBox" type="checkbox" default= "unchecked"/>  Apply to Job 2 </li>
-        <li className="checkboxComplete"><input className="checkedBox" type="checkbox" default= "unchecked"/> Apply to Job 3 </li>
+        <li className="checkboxComplete"><input className="checkedBox" type="checkbox" />  Apply to Job 2 </li>
+        <li className="done"><input className="checkedBox" type="checkbox" /> Done </li>
+
+        <li className="checkboxComplete"><input className="checkedBox" type="checkbox" /> Apply to Job 3 </li>
+        <li className="done"><input className="checkedBox" type="checkbox" /> Done </li>
 </ul>
       </div>
     )
